01b71acc14134deb97dcf4e00a68f68dd56971b9
Fer <reikafer@gmail.com>
2022-09-23 07:29:49 -0700

    Initial Commit

---
+++ prototipo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prototipo
+//
+//  Created by Alumno on 9/23/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
